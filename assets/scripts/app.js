fce4dc3f328b3a959d0a7028ccfa80c931cc35d8
ttamsmas <ttamsmas@gmail.com>
2020-10-15 13:29:26 -0700

    Added Event Listeners & Additional JS Pages

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -6,6 +6,10 @@
 // use require without a reference to ensure a file is bundled
 // require('./example')
 
+const events = require('./authorization/events.js')
+
 $(() => {
-  // your JS code goes here
+  $('#sign-up-form').on('submit', events.onSignUp)
+  $('#sign-in-form').on('submit', events.onSignIn)
+  $('#change-password-form').on('submit', events.onUpdatePassword)
 })

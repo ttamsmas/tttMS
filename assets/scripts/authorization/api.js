d155d0e05c9e8360ac3f05a84ad0402d6c1b2479
ttamsmas <ttamsmas@gmail.com>
2020-10-15 13:40:00 -0700

    Adding Authorization Functions from AJAX Lesson Need to correct how store.js is constructed, not sure if it needs keys or if they will autopopulate

--- assets/scripts/authorization/api.js
+++ assets/scripts/authorization/api.js
@@ -1,1 +1,35 @@
-// will add content later
+'use strict'
+
+const config = require('./../config')
+const store = require('./../store.js')
+
+const signUp = function (data) {
+  return $.ajax({
+    url: config.apiUrl + '/sign-up',
+    method: 'POST',
+    data: data
+  })
+}
+
+const signIn = function (data) {
+  return $.ajax({
+    url: config.apiUrl + '/sign-in',
+    method: 'POST',
+    data: data
+  })
+}
+
+const updatePassword = function (data) {
+  return $.ajax({
+    url: config.apiUrl + '/change-password',
+    headers: {'Authorization': 'Bearer ' + store.user.token},
+    method: 'PATCH',
+    data: data
+  })
+}
+
+module.exports = {
+  signUp,
+  signIn,
+  updatePassword
+}
--- assets/scripts/authorization/events.js
+++ assets/scripts/authorization/events.js
@@ -1,1 +1,43 @@
-// will fill out later
+'use strict'
+
+const getFormFields = require('./../../../lib/get-form-fields')
+const ui = require('./ui')
+const api = require('./api')
+
+const onSignUp = function (event) {
+  event.preventDefault()
+  // get the form from the event
+  const form = event.target
+  // get the data from the form
+  const data = getFormFields(form)
+  // send the data to the api
+  api.signUp(data)
+    // handle successful response
+    .then(ui.signUpSuccess)
+    // handle failed response
+    .catch(ui.signUpFailure)
+}
+
+const onSignIn = function (event) {
+  event.preventDefault()
+  const form = event.target
+  const data = getFormFields(form)
+  api.signIn(data)
+    .then(ui.signInSuccess)
+    .catch(ui.signInFailure)
+}
+
+const onUpdatePassword = function (event) {
+  event.preventDefault()
+  const form = event.target
+  const data = getFormFields(form)
+  api.updatePassword(data)
+    .then(ui.updateSuccess)
+    .catch(ui.updateFailure)
+}
+
+module.exports = {
+  onSignUp,
+  onSignIn,
+  onUpdatePassword
+}

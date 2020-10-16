7bca64e73aaf2045622473097ebb6317354e43b7
ttamsmas <ttamsmas@gmail.com>
2020-10-16 11:12:45 -0700

    Finishing Touches on New Game, Get Game by ID, and Get Games cURL

---
+++ curl-scripts/getgame.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+API="https://tic-tac-toe-api-development.herokuapp.com"
+URL_PATH="/games"
+
+curl "${API}${URL_PATH}" \
+  --include \
+  --request GET \
+  --header "Authorization: Bearer ${TOKEN}" \
+  --data '{
+    "games": {}
+  }'
+
+echo
+
+// TOKEN="" sh curl-scripts/getgame.sh
